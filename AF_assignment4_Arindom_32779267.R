1db699b3e4e2f8dd9cab0510081e1d86a9253885
arinbaruah <abar0090@student.monash.edu>
2024-05-03 15:23:48 +1000

    Initial 0s removed

--- AF_assignment4_Arindom_32779267.R
+++ AF_assignment4_Arindom_32779267.R
@@ -2,8 +2,8 @@
 generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   # Generate errors
   error <- rnorm(n, mean = 0, sd = sigma)
-  # Set up vector for the response with initial values set to 0
   
+  # Common errors to handle 
   if (any(length(phi) == 0) | any(is.na(phi)) | any(is.null(phi))) {
     phi = 0
   } 
@@ -11,15 +11,14 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   if (any(length(theta) == 0) | any(is.na(theta)) | any(is.null(theta))) {
     theta = 0
   }
-  
+  # Check if stationary
   if (max(abs(phi))>1) {
     stop("Function is not stationary")
   }
-  
+  # Check if invertible
   if (max(abs(theta))>1) {
     stop("Function is not invertible")
   }
-  
   p <- length(phi)
   q <- length(theta)
   y <- rep(0, n)
@@ -31,29 +30,31 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   
   expression <- ""  # Initialize expression as an empty character string
   
+  # Perform AR calculation
   j <- 1
   for (j in seq(1,j <= p)){
-    #final_phi <- final_phi + phi[j] * y[i - j]
     expression <- paste(expression, "+ phi[", j, "] * y[i -", j, "]", sep = "")
     j <- j + 1
   }
   
   phi_expression <- substring(expression, 3)
   
+  # Perform MA calculation
   j <- 1
   for (j in seq(1,j <= q)) {
-    #final_theta <- final_theta + theta[j]*error[i-j]
     expression <- paste(expression, "+ theta[", j, "] * error[i -", j, "]", sep = "")
     j <- j + 1
   }
   
   theta_expression <- substring(expression, 3)
-  
-  # Generate remaining observations
+  y <- rep(0, n)
+  # Add constant and error to ARMA calculation
   for (i in seq(k + 1, length = n - k)) {
     y[i] <- eval(parse(text = paste(phi_expression, "+", theta_expression, "+ error[i] + c", sep = "")))
   }
-  return(y[y!=0])
-}
+  # Remove initial observations with 0s by printing from k+1 to n
+  return(y[(k + 1) : n])
+} 
+
 
 
