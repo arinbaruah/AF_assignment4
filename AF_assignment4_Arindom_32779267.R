b4670c192c1c5f0456aa700c9b12dccebc3e9008
arinbaruah <abar0090@student.monash.edu>
2024-05-03 17:32:40 +1000

    Extra observations added for burn in

--- AF_assignment4_Arindom_32779267.R
+++ AF_assignment4_Arindom_32779267.R
@@ -1,7 +1,5 @@
 
 generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
-  # Generate errors
-  error <- rnorm(n, mean = 0, sd = sigma)
   
   # Common errors to handle 
   if (any(length(phi) == 0) | any(is.na(phi)) | any(is.null(phi))) {
@@ -11,10 +9,12 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   if (any(length(theta) == 0) | any(is.na(theta)) | any(is.null(theta))) {
     theta = 0
   }
+  
   # Check if stationary
   if (max(abs(phi))>1) {
     stop("Function is not stationary")
   }
+  
   # Check if invertible
   if (max(abs(theta))>1) {
     stop("Function is not invertible")
@@ -22,6 +22,10 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   p <- length(phi)
   q <- length(theta)
   y <- rep(0, n)
+  
+  # Generate errors with extra observations
+  error <- rnorm(n + 200, mean = 0, sd = sigma)
+  
   if (p >= q) {
     k <- p
   } else {
@@ -40,6 +44,8 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   phi_expression <- substring(expression, 3)
   
   # Perform MA calculation
+  expression <- ""  # Initialize expression as an empty character string
+  
   j <- 1
   for (j in seq(1,j <= q)) {
     expression <- paste(expression, "+ theta[", j, "] * error[i -", j, "]", sep = "")
@@ -47,14 +53,23 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   }
   
   theta_expression <- substring(expression, 3)
-  y <- rep(0, n)
-  # Add constant and error to ARMA calculation
-  for (i in seq(k + 1, length = n - k)) {
+  
+  # Add constant and error to ARMA sum calculation with extra observations
+  for (i in seq(k + 1, length = n + k)) {
     y[i] <- eval(parse(text = paste(phi_expression, "+", theta_expression, "+ error[i] + c", sep = "")))
   }
   # Remove initial observations with 0s by printing from k+1 to n
-  return(y[(k + 1) : n])
+  return(y[k + 1 : n])
 } 
 
+
+
+library(fpp3)
+n <- 50
+p <- 1
+q <- 1
+
+tsibble(time = 1 : 100, y = generate_arma(n = 100, c=1, phi=0.3, theta = c(0.2,0.3),index = time)) |>
+  gg_tsdisplay(y,plot_type = "partial")
 
 
