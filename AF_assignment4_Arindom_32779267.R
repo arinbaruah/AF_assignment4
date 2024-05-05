573f95e523461508b70debd579f0da7921ec0436
arinbaruah <abar0090@student.monash.edu>
2024-05-05 15:23:58 +1000

    Changes to invertibility and stationary condition added

--- AF_assignment4_Arindom_32779267.R
+++ AF_assignment4_Arindom_32779267.R
@@ -11,13 +11,15 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   }
   
   # Check if stationary
-  if (max(abs(phi))>1) {
-    stop("Function is not stationary")
+  
+  if (any(abs(polyroot(c(1,-phi))) <= 1)) {
+    stop("Model is not stationary")
   }
   
   # Check if invertible
-  if (max(abs(theta))>1) {
-    stop("Function is not invertible")
+  
+  if (any(abs(polyroot(c(1,theta))) <= 1)) {
+    stop("Model is not invertible")
   }
   p <- length(phi)
   q <- length(theta)
@@ -27,9 +29,9 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   error <- rnorm(n + 200, mean = 0, sd = sigma)
   
   if (p >= q) {
-    k <- p
+    max_component <- p
   } else {
-    k <- q
+    max_component <- q
   }
   
   expression <- ""  # Initialize expression as an empty character string
@@ -43,8 +45,11 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   
   phi_expression <- substring(expression, 3)
   
-  # Perform MA calculation
+
+
   expression <- ""  # Initialize expression as an empty character string
+  
+  # Perform MA calculation
   
   j <- 1
   for (j in seq(1,j <= q)) {
@@ -55,21 +60,12 @@ generate_arma <- function(n = 100, c = 0, phi = NULL, theta = NULL, sigma = 1) {
   theta_expression <- substring(expression, 3)
   
   # Add constant and error to ARMA sum calculation with extra observations
-  for (i in seq(k + 1, length = n + k)) {
+  
+  for (i in seq(max_component + 1, length = n + max_component)) {
     y[i] <- eval(parse(text = paste(phi_expression, "+", theta_expression, "+ error[i] + c", sep = "")))
   }
-  # Remove initial observations with 0s by printing from k+1 to n
-  return(y[k + 1 : n])
+  
+  # Remove initial observations with 0s by printing from (max_component + 1) to n 
+  return(y[max_component + 1 : n])
 } 
-
-
-
-library(fpp3)
-n <- 50
-p <- 1
-q <- 1
-
-tsibble(time = 1 : 100, y = generate_arma(n = 100, c=1, phi=0.3, theta = c(0.2,0.3),index = time)) |>
-  gg_tsdisplay(y,plot_type = "partial")
-
 
